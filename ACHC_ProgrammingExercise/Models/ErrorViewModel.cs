a9c9ba8d95e8cc0008bb886d366043d2edc118de
DESKTOP-7B3O0B3\ichar <richardchiqui3@yahoo.com>
2023-04-26 20:24:35 -0400

    Init commitL Setup project by adding .Net and react folders/files

---
+++ ACHC_ProgrammingExercise/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ACHC_ProgrammingExercise.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
