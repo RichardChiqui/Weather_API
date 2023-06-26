c941895ddbbefd6945db36f8ca6e5adabde4bf5f
DESKTOP-7B3O0B3\ichar <richardchiqui3@yahoo.com>
2023-06-25 21:45:39 -0400

    used weather api tool

--- ACHC_ProgrammingExercise/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ACHC_ProgrammingExercise.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
